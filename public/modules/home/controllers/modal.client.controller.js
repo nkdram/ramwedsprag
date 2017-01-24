2f11ffaa08fbf467616f35c9db10056f8876e209
nkdram <nkdramkumar@gmail.com>
2017-01-24 19:33:22 +0530

    Locations

--- public/modules/home/controllers/modal.client.controller.js
+++ public/modules/home/controllers/modal.client.controller.js
@@ -147,7 +147,7 @@
         $scope.refreshAddresses = function(address) {
             var params = {address: address, sensor: false};
             return $http.get(
-                'http://maps.googleapis.com/maps/api/geocode/json',
+                'https://maps.googleapis.com/maps/api/geocode/json',
                 {params: params}
             ).then(function(response) {
                     $scope.addresses = response.data.results;
